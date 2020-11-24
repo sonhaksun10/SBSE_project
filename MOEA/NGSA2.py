e744e07701b11739ef531e49937445939ee892ea
sonhaksun10 <sonhaksun10@gmail.com>
2020-11-24 17:20:42 +0900

    lastly committed by sonhaksun10

--- MOEA/NGSA2.py
+++ MOEA/NGSA2.py
@@ -23,9 +23,11 @@ def run_NSGA2():
         new_pop = population
         evaluate(new_pop)
         pareto = get_pareto(new_pop)
-        new_pop = select(new_pop, pareto)
+        new_pop = select(pareto)
         population = new_pop
         population_history.append(population)
+
+    first_pareto = get_first_pareto(population) #final result
 
 
 def initial_genes():
@@ -44,10 +46,41 @@ def crossover(pop):
     new_pop = pop[:]
 
 def evaluate(pop):
+    '''
+    complete this function after feature detection team finished their work
+
+    :param pop:
+    :return:
+    '''
     pass
 
 def get_pareto(pop):
+    '''
+    important!
+    get pareto front with new data
+
+    return type: [first pareto, second pareto, ... ,last pareto]
+    ex) first pareto = [gene3, gene15, gene19, gene22, ...]
+        second pareto = [gene5, gene10, gene20, ...]
+
+    :param pop:
+    :return:    list(list(Gene_Info))
+    '''
     pass
 
-def select(pop,pareto):
+def get_first_pareto(pop):
+    '''
+    get only first pareto
+
+    :param pop:
+    :return:
+    '''
+
+def select(pareto):
+    '''
+    select certain number of genes
+
+    :param pareto:
+    :return:
+    '''
     pass
